fd680b7f5863acf1712f28fffaa7d7c7a07e134c
3kenstain <3kenstain@gmail.com>
2026-05-22 09:04:00 +0300

    chore: remove temp TG debug logging

--- backend/app/Http/Controllers/TelegramController.php
+++ backend/app/Http/Controllers/TelegramController.php
@@ -25,16 +25,6 @@ class TelegramController extends Controller
     {
         $update = $request->all();
 
-        // Временно: логируем входящие для отладки chat_id
-        $msg = $update['message'] ?? null;
-        if ($msg) {
-            Log::error('TG_DEBUG incoming', [
-                'chat_id'  => $msg['chat']['id'] ?? null,
-                'username' => $msg['chat']['username'] ?? null,
-                'text'     => $msg['text'] ?? null,
-            ]);
-        }
-
         if (isset($update['message'])) {
             $this->handleMessage($update['message']);
         }
